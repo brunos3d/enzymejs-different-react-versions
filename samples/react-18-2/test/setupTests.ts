2d31d386b3a22c4633554c0bf36401abf26bd2e0
Bruno Silva <bruno3dcontato@gmail.com>
2023-02-15 15:44:13 -0300

    chore: define dom entities in global object

--- samples/react-18-2/test/setupTests.ts
+++ samples/react-18-2/test/setupTests.ts
@@ -1,4 +1,21 @@
+/* global globalThis */
+import './setupJsdomMocks';
+import { JSDOM } from 'jsdom';
 import { configure } from 'enzyme';
 import ReactEighteenAdapter from '@cfaester/enzyme-adapter-react-18';
+
+// Setup JSDOM
+const dom = new JSDOM('', {
+  // Enable `requestAnimationFrame` which Preact uses internally.
+  pretendToBeVisual: true,
+});
+
+globalThis.Event = dom.window.Event;
+globalThis.Node = dom.window.Node;
+
+// @ts-ignore
+globalThis.window = dom.window;
+globalThis.document = dom.window.document;
+globalThis.requestAnimationFrame = dom.window.requestAnimationFrame;
 
 configure({ adapter: new ReactEighteenAdapter() });
